05868e7f4d95f970ef0167f2a335295897581a3a
Lucas Moutinho <lucas.silvamoutinho@gmail.com>
2018-11-04 19:01:08 -0200

    setup inicial

---
+++ castaway.c
@@ -0,0 +1,21 @@
+/*
+
+ALUNO: LUCAS DA SILVA MOUTINHO - 15/0015747
+PROFESSOR: ALCHIERI
+ESTUDO DIRIGIDO 8 - PROGRAMAÇÃO CONCORRENTE - 2/2018
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+#define TRUE 1
+
+
+int main(){
+
+  return 0;
+}
